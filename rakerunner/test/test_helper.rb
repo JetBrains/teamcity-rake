7877b2b3580cfa434adaff86bbeb3c3aad36f1ab
Roman.Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-01-05 19:40:37 +0000

    1. test helper was updated 2. autorunner was moved to appropriate location

--- rakerunner/test/test_helper.rb
+++ rakerunner/test/test_helper.rb
@@ -3,7 +3,9 @@
 # Date: 27.12.2007
 # Time: 21:37:42
 # To change this template use File | Settings | File Templates.
-ENV["idea.rake.debug"] = "true"
+ENV["idea.rake.debug.sources"] = "true"
+ENV["idea.rake.debug.log"] = "true"
+
 $: << File.expand_path(File.dirname(__FILE__) + "/..")
 
 require "test/unit"
