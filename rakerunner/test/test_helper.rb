f6752eaad573bf169235a9b450756502bd35d9f4
Roman.Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-01-05 18:14:51 +0000

    We can't add "src" direcory to path, because it overrides test runner pathes ("test/unit/...") on agent! This affects installed rake-runner plugin on agent machine!

--- rakerunner/test/test_helper.rb
+++ rakerunner/test/test_helper.rb
@@ -3,4 +3,4 @@
 # Date: 27.12.2007
 # Time: 21:37:42
 # To change this template use File | Settings | File Templates.
-$: << File.expand_path(File.dirname(__FILE__) + "/../src")
+$: << File.expand_path(File.dirname(__FILE__) + "/..")
