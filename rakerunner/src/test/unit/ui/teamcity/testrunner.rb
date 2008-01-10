957bc14d9a2c1200af7835a06c63bcd8700d921f
Roman.Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-01-10 08:58:38 +0000

    Code was refactored

--- rakerunner/src/test/unit/ui/teamcity/testrunner.rb
+++ rakerunner/src/test/unit/ui/teamcity/testrunner.rb
@@ -32,75 +32,66 @@ else
   require 'test/unit/ui/teamcity/event_queue/event_queue'
 end
 
-module Test
-  module Unit
-    module UI
-      module TeamCity
+# Runs a Test::Unit::TestSuite on teamcity server.
+class Test::Unit::UI::TeamCity::TestRunner
+  extend TestRunnerUtilities
 
-        # Runs a Test::Unit::TestSuite on teamcity server.
-        class TestRunner
-          extend TestRunnerUtilities
+  # Includes module with event handlers
+  include EventHandlers
 
-          # Includes module with event handlers
-          include Test::Unit::UI::TeamCity::EventHandlers
+  # Creates a new TestRunner for running the passed
+  # suite.
+  def initialize(suite, output_level=NORMAL)
 
-          # Creates a new TestRunner for running the passed
-          # suite.
-          def initialize(suite, output_level=NORMAL)
-
-            if (suite.respond_to?(:suite))
-              @suite = suite.suite
-            else
-              @suite = suite
-            end
-            @result = nil
-          end
-
-
-          # Starts testing
-          def start
-            setup_mediator
-            attach_to_mediator
-
-            msg_dispatcher =  Rake::TeamCity.msg_dispatcher
-            manual_start = !msg_dispatcher.started?
-
-            # open xml-rpc connection
-            msg_dispatcher.start_dispatcher if manual_start
-
-            start_mediator
-
-            # close xml-rpc connection
-            msg_dispatcher.stop_dispatcher if manual_start
-
-            @result
-          end
-
-          def start_mediator
-            return @mediator.run_suite
-          end
-
-          private
-
-          def setup_mediator
-            @mediator = TestRunnerMediator.new(@suite)
-            @suite_name = (@suite.kind_of?(Module) ? @suit.name : @suite.to_s)            
-          end
-
-          def attach_to_mediator
-            @mediator.add_listener(TestResult::FAULT, &method(:add_fault))
-            @mediator.add_listener(TestResult::CHANGED, &method(:result_changed))
-
-            @mediator.add_listener(TestCase::STARTED, &method(:test_started))
-            @mediator.add_listener(TestCase::FINISHED, &method(:test_finished))
-
-            @mediator.add_listener(TestRunnerMediator::STARTED, &method(:started))
-            @mediator.add_listener(TestRunnerMediator::FINISHED, &method(:finished))
-            @mediator.add_listener(TestRunnerMediator::RESET, &method(:reset_ui))
-          end
-        end
-      end
+    if (suite.respond_to?(:suite))
+      @suite = suite.suite
+    else
+      @suite = suite
     end
+    @result = nil
+  end
+
+
+  # Starts testing
+  def start
+    setup_mediator
+    attach_to_mediator
+
+    msg_dispatcher =  Rake::TeamCity.msg_dispatcher
+    manual_start = !msg_dispatcher.started?
+
+    # open xml-rpc connection
+    msg_dispatcher.start_dispatcher if manual_start
+
+    start_mediator
+
+    # close xml-rpc connection
+    msg_dispatcher.stop_dispatcher if manual_start
+
+    @result
+  end
+
+  def start_mediator
+    return @mediator.run_suite
+  end
+
+  private
+
+  def setup_mediator
+    @mediator = TestRunnerMediator.new(@suite)
+    @suite_name = (@suite.kind_of?(Module) ? @suit.name : @suite.to_s)
+  end
+
+  def attach_to_mediator
+    @mediator.add_listener(TestResult::FAULT, &method(:add_fault))
+    @mediator.add_listener(TestResult::CHANGED, &method(:result_changed))
+
+    @mediator.add_listener(TestCase::STARTED, &method(:test_started))
+    @mediator.add_listener(TestCase::FINISHED, &method(:test_finished))
+
+    @mediator.add_listener(TestRunnerMediator::STARTED, &method(:started))
+    @mediator.add_listener(TestRunnerMediator::FINISHED, &method(:finished))
+    @mediator.add_listener(TestRunnerMediator::RESET, &method(:reset_ui))
   end
 end
 
