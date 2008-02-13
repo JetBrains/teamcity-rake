95d08d9a261fbeb86ff0f17ae6a26d41c8a94839
Roman.Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-02-13 14:52:28 +0000

    Test data updated : Pending spec was updated

--- rakerunner/test_data/spec/common/pending_sample_spec.rb
+++ rakerunner/test_data/spec/common/pending_sample_spec.rb
@@ -11,7 +11,7 @@ describe "PendingTests" do
   it "should be IGNORED in teamcity - ExamplePendingError exception with msg" do
 
     raise Spec::Example::ExamplePendingError, "ExamplePendingError was raised", caller
-    end
+  end
 
   it "should be IGNORED in teamcity - ExamplePendingError exception without msg" do
 
@@ -21,5 +21,30 @@ describe "PendingTests" do
   it "should be IGNORED in teamcity - ExamplePendingError exception without msg and stacktrace" do
 
     raise Spec::Example::ExamplePendingError
+  end
+
+  it 'should raise an ExamplePendingError if no block is supplied' do
+    lambda {
+      include Pending
+      pending "TODO"
+    }.should raise_error(ExamplePendingError, /TODO/)
+  end
+
+  it 'should raise an ExamplePendingError if a supplied block fails as expected' do
+    lambda {
+      include Pending
+      pending "TODO" do
+        raise "oops"
+      end
+    }.should raise_error(ExamplePendingError, /TODO/)
+  end
+
+  it 'should raise a PendingExampleFixedError if a supplied block starts working' do
+    lambda {
+      include Pending
+      pending "TODO" do
+        # success!
+      end
+    }.should raise_error(PendingExampleFixedError, /TODO/)
   end
 end
