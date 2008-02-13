5e53c82277790f5fb022dc9b440fa4a3d7d9b3fd
Roman.Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-02-13 15:13:31 +0000

    Test data updated : Pending spec was updated

--- rakerunner/test_data/spec/common/pending_sample_spec.rb
+++ rakerunner/test_data/spec/common/pending_sample_spec.rb
@@ -6,19 +6,19 @@
 require "spec"
 
 describe "PendingTests" do
-  it "should be IGNORED in teamcity - without block"
+  it 'should be IGNORED in teamcity - without block'
 
-  it 'should raise an ExamplePendingError if no block is supplied' do
+  it 'should be IGNORED in teamcity - raise an ExamplePendingError if no block is supplied' do
     pending "TODO"
   end
 
-  it 'should raise an ExamplePendingError if a supplied block fails as expected' do
+  it 'should be IGNORED in teamcity - raise an ExamplePendingError if a supplied block fails as expected' do
     pending "TODO (Exception)" do
       raise "Medved Exception"
     end
   end
 
-  it 'should raise a PendingExampleFixedError if a supplied block starts working' do
+  it 'should be IGNORED in teamcity - raise a PendingExampleFixedError if a supplied block starts working' do
     pending "TODO" do
       # Do nothing
     end
