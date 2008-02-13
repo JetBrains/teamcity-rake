536f80125d99f9fa988b1e3bd697be59a00242b7
Roman.Chernyatchik <Roman.Chernyatchik@jetbrains.com>
2008-02-13 15:07:36 +0000

    Test data updated : Pending spec was updated

--- rakerunner/test_data/spec/common/pending_sample_spec.rb
+++ rakerunner/test_data/spec/common/pending_sample_spec.rb
@@ -8,45 +8,19 @@ require "spec"
 describe "PendingTests" do
   it "should be IGNORED in teamcity - without block"
 
-  it "should be IGNORED in teamcity - ExamplePendingError exception with msg" do
-
-    raise Spec::Example::ExamplePendingError, "ExamplePendingError was raised", caller
+  it 'should raise an ExamplePendingError if no block is supplied' do
+    pending "TODO"
   end
 
-  it "should be IGNORED in teamcity - ExamplePendingError exception without msg" do
-
-    raise Spec::Example::ExamplePendingError, caller
+  it 'should raise an ExamplePendingError if a supplied block fails as expected' do
+    pending "TODO" do
+      raise "Medved Exception"
+    end
   end
 
-  it "should be IGNORED in teamcity - ExamplePendingError exception without msg and stacktrace" do
-
-    raise Spec::Example::ExamplePendingError
-  end
-end
-
-
-
-module Spec
-  module DSL
-    describe "Pending" do
-      it 'should raise an ExamplePendingError if no block is supplied' do
-        include Pending
-        pending "TODO"
-      end
-
-      it 'should raise an ExamplePendingError if a supplied block fails as expected' do
-        include Pending
-        pending "TODO" do
-          raise "oops"
-        end
-      end
-
-      it 'should raise a PendingExampleFixedError if a supplied block starts working' do
-        include Pending
-        pending "TODO" do
-          # success!
-        end
-      end
+  it 'should raise a PendingExampleFixedError if a supplied block starts working' do
+    pending "TODO" do
+      # Do nothing
     end
   end
 end
