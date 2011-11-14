4e5665c276ec913c979b7d92517e131c328e2da0
Vladislav.Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2011-11-14 15:50:59 +0000

    Rake-runner tests: bash scripts editing

--- rake-runner-test/testData/plugins/rakeRunner/gems/initializer.sh
+++ rake-runner-test/testData/plugins/rakeRunner/gems/initializer.sh
@@ -1,31 +1,77 @@
 #!/bin/bash
 
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
-# must be in $HOME/.bash_profile
-source $HOME/.bash_profile
+echo "##teamcity[blockOpened name='Checking RVM executable']"
+source $HOME/.bash_profile # TODO: may be not needed, but withot this line some bugs occured
+
+if [[ -z `rvm` ]]; then
+    # Try to use default .bash_profile
+    if [[ -s "$HOME/.bash_profile" ]]; then
+        source $HOME/.bash_profile
+    fi
+fi
+if [[ -z `rvm` ]]; then
+    # Create & Use manual default .bash_profile
+    echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && . \"$HOME/.rvm/scripts/rvm\"" > .bash_profile
+    source .bash_profile
+    rm .bash_profile
+fi
+if [[ -z `rvm` ]]; then
+    # Cannot locate rvm! must fail build
+    echo "##teamcity[message text='Cannot locate rvm!' status='FAILURE']"
+    exit 1
+fi
+echo "##teamcity[blockClosed name='Checking RVM executable']"
 
 
 if [[ -z "$RR_RUBY_VERSION" ]]; then
     echo "You must specify ruby version as RR_RUBY_VERSION variable"
+    echo "##teamcity[message text='RR_RUBY_VERSION are empty' errorDetails='You must specify ruby version as RR_RUBY_VERSION variable' status='ERROR']"
     exit 1
 fi
+
+# Check for minimal environment
+rvm use $RR_RUBY_VERSION || (echo "##teamcity[message text='No such rubies version |'$RR_RUBY_VERSION|'' status='FAILURE']" && exit 1)
+rvm use $RR_RUBY_VERSION@global
+gem install bundler
 
 if [[ -z "$rrprefix" ]]; then
     rrprefix=""
 fi
+echo "##teamcity[message text='Using \$rrprefix = |'$rrprefix|'' status='NORMAL']"
 
+echo "##teamcity[blockOpened name='Creating Gemsets']"
 for i in `ls -d */ | grep -v vendor`; do
     pushd $i
+    echo "##teamcity[blockOpened name='Processing $i']"
 
+
+    echo "##teamcity[blockOpened name='Copying cache']"
     mkdir -p vendor/cache
     cp -rv ../vendor/cache/* vendor/cache/
+    echo "##teamcity[blockClosed name='Copying cache']"
 
+
+    echo "##teamcity[blockOpened name='Creating gemset |'$RR_RUBY_VERSION@$rrprefix$i|'']"
     rvm use "$RR_RUBY_VERSION@$rrprefix$i" --create
-    bundle install
+    echo "##teamcity[blockClosed name='Creating gemset |'$RR_RUBY_VERSION@$rrprefix$i|'']"
 
+    echo "##teamcity[blockOpened name='Execute Bundler']"
+    if [[ -n "$bundle_repository_local" ]]; then
+        echo "##teamcity[message text='Using bundle only with local repository' status='NORMAL']"
+        bundle install --local
+    else
+        bundle install
+    fi
+    echo "##teamcity[blockClosed name='Execute Bundler']"
+
+    echo "##teamcity[blockOpened name='Updating cache']"
     bundle pack
     mv -v vendor/cache/* ../vendor/cache/
     rm -rf vendor
+    echo "##teamcity[blockClosed name='Updating cache']"
+
+    echo "##teamcity[blockClosed name='Processing $i']"
     popd
 done
+echo "##teamcity[blockClosed name='Creating Gemsets']"
 
--- rake-runner-test/testData/plugins/rakeRunner/gems/remover.sh
+++ rake-runner-test/testData/plugins/rakeRunner/gems/remover.sh
@@ -1,23 +1,44 @@
 #!/bin/bash
 
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
-# must be in $HOME/.bash_profile
-source $HOME/.bash_profile
+echo "##teamcity[blockOpened name='Checking RVM executable']"
+if [[ -z `rvm` ]]; then
+    # Try to use default .bash_profile
+    if [[ -s "$HOME/.bash_profile" ]]; then
+        source $HOME/.bash_profile
+    fi
+fi
+if [[ -z `rvm` ]]; then
+    # Create & Use manual default .bash_profile
+    echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && . \"$HOME/.rvm/scripts/rvm\"" > .bash_profile
+    source .bash_profile
+    rm .bash_profile
+fi
+if [[ -z `rvm` ]]; then
+    # Cannot locate rvm! must fail build
+    echo "##teamcity[message text='Cannot locate rvm!' status='FAILURE']"
+    exit 1
+fi
+echo "##teamcity[blockClosed name='Checking RVM executable']"
 
 
-if [[ -z "$RUBY_VERSION" ]]; then
-    echo "You must specify ruby version"
+if [[ -z "$RR_RUBY_VERSION" ]]; then
+    warn "You must specify ruby version as RR_RUBY_VERSION variable"
+    echo "##teamcity[message text='RR_RUBY_VERSION are empty' errorDetails='You must specify ruby version as RR_RUBY_VERSION variable' status='ERROR']"
     exit 1
 fi
 
 if [[ -z "$rrprefix" ]]; then
     rrprefix=""
 fi
+echo "##teamcity[message text='Using \$rrprefix = |'$rrprefix|'' status='NORMAL']"
 
-for i in `ls -d */`; do
+echo "##teamcity[blockOpened name='Deleting Gemsets']"
+for i in `ls -d */ | grep -v vendor`; do
     pushd $i
+    echo "##teamcity[blockOpened name='Removing gemset |'$RR_RUBY_VERSION@$rrprefix$i|'']"
     rvm use "$RUBY_VERSION"
     rvm --force gemset delete "$rrprefix$i"
+    echo "##teamcity[blockClosed name='Removing gemset |'$RR_RUBY_VERSION@$rrprefix$i|'']"
     popd
 done
-
+echo "##teamcity[blockClosed name='Deleting Gemsets']"
