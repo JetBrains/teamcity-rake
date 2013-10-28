f65ed8366e7a9c80700613442d0602c789e3d9c9
Maxim.Manuylov <Maxim.Manuylov@jetbrains.com>
2013-10-28 21:58:06 +0000

    fix "Unused import" inspections

--- rake-runner-test/src/jetbrains/buildServer/agent/ruby/rvm/InstalledRVMTest.java
+++ rake-runner-test/src/jetbrains/buildServer/agent/ruby/rvm/InstalledRVMTest.java
@@ -11,7 +11,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
